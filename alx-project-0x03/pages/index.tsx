ff46997fc1e09d0822d0d08e8c4628230d95195e
kayden14 <faithtega14@gmail.com>
2025-06-16 03:11:33 -0700

    Re-added full project as normal folder

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/layouts/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="text-center py-20">
+        <h1 className="text-4xl font-bold">Welcome to Splash App</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;

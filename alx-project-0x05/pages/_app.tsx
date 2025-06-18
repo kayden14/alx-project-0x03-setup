e7dd6debff411c264246d2f31c92d9c4b1a3faa4
kayden14 <faithtega14@gmail.com>
2025-06-18 02:33:22 -0700

    Re-added full project as normal folder

---
+++ alx-project-0x05/pages/_app.tsx
@@ -0,0 +1,16 @@
+import Layout from "@/components/layouts/Layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { CountProvider } from "@/context/CountContext";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
+}
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
